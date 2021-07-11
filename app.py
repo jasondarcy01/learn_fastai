bed96a898a8cadf7ec588205a1940700e55ed277
Jason Darcy <darchcruise@Jasons-2020-MacBook-Air.local>
2021-07-10 20:33:14 -0400

    web_app

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import os
+import pickle
+import PIL
+from PIL import Image
+import fastbook
+from fastbook import *
+from fastai.vision.widgets import *
+from fastai.vision import *
+import torchvision.transforms as T
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def hello_world():
+	if request.method=="POST":
+		file = request.files["file"]
+		file.save(os.path.join("uploads", file.filename))
+		learn_inf = load_learner('export.pkl')
+		print(os.path.join("uploads", file.filename))
+		img = PILImage.create(os.path.join("uploads", file.filename))
+		#print(type(img))
+		pred,pred_idx,probs = learn_inf.predict(img)
+		return render_template("index.html", message=f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(debug=True)
